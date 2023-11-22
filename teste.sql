c124cd62fc2bc5589db7b1d6e74ef12da4005c42
Levine Caixeta <47031195+islbnx@users.noreply.github.com>
2023-11-22 06:57:06 -0300

    Juntando várias Tabelas com SQL

--- teste.sql
+++ teste.sql
@@ -1,93 +1,51 @@
-WITH tab2020 AS (
-    SELECT 
-        nome_completo,
-        email,
-        tipo_do_produto,
-        SUM(faturamento) AS faturamento_total_20,
-        SUM(qtd_vendida) AS qtds_vendida_20
-    FROM tabela_vendas
-    WHERE ano_venda = 2020
-        AND genero IN ("Masculino", "Feminino")
-        AND estado_civil IN ("Casado", "Solteiro")
-        AND nivel_escolar IN ("Superior Completo", "Superior Incompleto")
-        AND continente = 'América do Norte'
-        AND pais = 'Estados Unidos'
-        AND tipo = 'Física'
-    GROUP BY nome_completo, email, tipo_do_produto
-),
-tab2021 AS (
-    SELECT 
-        nome_completo,
-        email,
-        tipo_do_produto,
-        SUM(faturamento) AS faturamento_total_21,
-        SUM(qtd_vendida) AS qtds_vendida_21
-    FROM tabela_vendas
-    WHERE ano_venda = 2021
-        AND genero IN ("Masculino", "Feminino")
-        AND estado_civil IN ("Casado", "Solteiro")
-        AND nivel_escolar IN ("Superior Completo", "Superior Incompleto")
-        AND continente = 'América do Norte'
-        AND pais = 'Estados Unidos'
-        AND tipo = 'Física'
-    GROUP BY nome_completo, email, tipo_do_produto
-),
-tabfull AS (
-    SELECT 
-        COALESCE(t20.nome_completo, t21.nome_completo) AS nome_completo,
-        COALESCE(t20.email, t21.email) AS email,
-        COALESCE(t20.tipo_do_produto, t21.tipo_do_produto) AS tipo_do_produtoo,
-        t20.faturamento_total_20 AS faturamento_total_2020,
-        t20.qtds_vendida_20 AS qtds_vendida_2020,
-        t21.faturamento_total_21 AS faturamento_total_2021,
-        t21.qtds_vendida_21 AS qtds_vendida_2021
-    FROM tab2020 t20
-    FULL JOIN tab2021 t21
-        ON t20.nome_completo = t21.nome_completo
-        AND t20.email = t21.email
-),
-cliente20 AS (
-    SELECT
-        tipo_do_produtoo AS produto_20,
-        COUNT(nome_completo) AS qtd_clientes_20,
-        SUM(faturamento_total_2020) AS faturamento_total_em_20
-    FROM tabfull
-    WHERE faturamento_total_2020 IS NOT NULL
-    GROUP BY tipo_do_produtoo
-),
-cliente21_novos AS (
-    SELECT 
-        tipo_do_produtoo AS produto_21,
-        COUNT(nome_completo) AS qtd_clientes_21_novo,
-        SUM(faturamento_total_2021) AS faturamento_total_em_21_novo
-    FROM tabfull
-    WHERE faturamento_total_2020 IS NULL
-        AND faturamento_total_2021 IS NOT NULL
-    GROUP BY tipo_do_produtoo
-),
-cliente21_retornando AS (
-    SELECT 
-        tipo_do_produtoo,
-        COUNT(nome_completo) AS qtd_clientes_21_retornando,
-        SUM(faturamento_total_2021) AS faturamento_total_em_21_retornando
-    FROM tabfull
-    WHERE faturamento_total_2020 IS NOT NULL
-        AND faturamento_total_2021 IS NOT NULL
-    GROUP BY tipo_do_produtoo
-),
-cliente21_NAO_retornando AS (
-    SELECT 
-        tipo_do_produtoo,
-        COUNT(nome_completo) AS qtd_clientes_21_NAO_retornando,
-        SUM(faturamento_total_2021) AS faturamento_total_em_21_NAO_retornando
-    FROM tabfull
-    WHERE faturamento_total_2020 IS NOT NULL
-        AND faturamento_total_2021 IS NULL
-    GROUP BY tipo_do_produtoo
-)
-SELECT 
-    *
-FROM cliente20
-CROSS JOIN cliente21_novos
-CROSS JOIN cliente21_retornando
-CROSS JOIN cliente21_NAO_retornando;
+-- Juntando várias tabelas
+WITH tab1 AS
+(
+       SELECT *
+       FROM   bd_vendas AS t1
+       JOIN   bd_cliente AS t2
+       ON     t1.id_cliente = t2.id_cliente
+       JOIN   bd_loja AS t3
+       ON     t1.id_loja = t3.id_loja
+       JOIN   bd_localidade AS t4
+       ON     t3.id_localidade = t4.id_localidade
+       JOIN   bd_produtos AS t5
+       ON     t1.sku = t5.sku ), 
+    tabelavendas AS
+        (
+       SELECT data_da_venda,
+              DAY(data_da_venda) AS dia_venda,
+              MONTH(data_da_venda) AS mes_venda,
+              YEAR(data_da_venda) AS ano_venda,
+              ordem_de_compra,
+              qtd_vendida,
+                     concat(primeiro_nome," ", sobrenome) AS nome_completo,
+              email,
+              CASE
+                     WHEN genero = 'F' THEN 'Feminino'
+                     WHEN genero = 'M' THEN 'Masculino'
+                     ELSE 'Outros'
+              END                                 AS genero,
+              year(now()) - year(data_nascimento) AS idade,
+              CASE
+                     WHEN estado_civil = 'S' THEN 'Solteiro'
+                     WHEN estado_civil = 'C' THEN 'Casado'
+                     ELSE 'Outro'
+              END AS estado_civil,
+              num_filhos,
+              nivel_escolar,
+              nome_da_loja,
+              tipo,
+              gerente_loja,
+              pais,
+              continente,
+              produto,
+              marca,
+              tipo_do_produto,
+              preco_unitario,
+              custo_unitario,
+              (qtd_vendida * preco_unitario) - custo_unitario AS faturamento,
+              observacao
+       FROM   tab1)
+SELECT *
+FROM   tabelavendas
